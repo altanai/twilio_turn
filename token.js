261ce3ff6fd277378f178b1312ecf00d3d980779
altanai <tara181989@gmail.com>
2021-08-25 21:46:14 -0700

    access control alow header

--- token.js
+++ token.js
@@ -18,6 +18,15 @@ var cors = require('cors');
 app.use(cors());
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({extended: true})); // for parsing application/x-www-form-urlencoded
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header("Access-Control-Allow-Methods","GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers","*");
+    next();
+});
+
 app.post('/token', cors(), (req, res, next) => {
     console.log("Token request received ");
     client.tokens.create()
